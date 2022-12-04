0ae0d9f2a9746ba27e22ec8d65d79e168e333ce1
AHMAD SHBAT <ahmad.shbat@continental-corporation.com>
2022-12-04 09:21:27 +0200

    finally

--- Py_poject1.py
+++ Py_poject1.py
@@ -10,7 +10,7 @@ class Product:
         self.name = name
 
     def __str__(self):
-        return json.dumps(self, ensure_ascii=False)
+        return self.__dict__
 
     def __repr__(self):
         return self.__str__()
@@ -23,9 +23,9 @@ class Categories(Product):
 
 
 class Amplifier(Categories):
-    def __init__(self, name: string, power: int, ChannelNumbers: int):
+    def __init__(self, name: string, power: int, ChannelNumbers: int, size:int):
         Product.__init__(self, name)
-        self.type = "AMP"
+        self.type = "amplifier"
         self.power = power
         self.channelNumbers = ChannelNumbers
 
@@ -33,88 +33,117 @@ class Amplifier(Categories):
 class Receiver(Categories):
     def __init__(self, name: string, size: int, color: string, ChannelNumbers: int):
         Product.__init__(self, name)
-        self.type = "AMP"
+        self.type = "receiver"
         self.size = size
         self.channelNumbers = ChannelNumbers
         self.color = color
 
     def __str__(self):
-        return json.dumps(self, ensure_ascii=False)
+        return self.__dict__
 
     def __repr__(self):
         return self.__str__()
 
-    def __iter__(self):
-        yield {
-            "name": self.name,
-            "size": self.size,
-            "color": self.color,
-            "ChannelNumbers": self.channelNumbers,
-            "type": self.type
-        }
+    # def __iter__(self):
+    #     yield {
+    #         "name": self.name,
+    #         "size": self.size,
+    #         "color": self.color,
+    #         "ChannelNumbers": self.channelNumbers,
+    #         "type": self.type
+    #     }
 
 
-class Orders(Product):
-    def __init__(self, name: string, type: string, size: int = 1, color: string = None, ChannelNumbers: int = None):
-        Product.__init__(self, name)
-        self.type = "AMP"
-        self.size = size
-        self.channelNumbers = ChannelNumbers
-        self.color = color
-
-
-def catagories():
+def AddProduct():
     print('''
-        choose option:
-            1. Add a category
-            2. Remove a category
-            3. Display all the categories
-        ''')
-    option: int = input(':')
-    match int(option):
-        case 1:
-            AddCategory();
-        case 2:
-            pass
-        case 3:
-            pass
+        type of product info:''')
+    Type = input('type:');
+    if not Type:
+        print('please insert string..');
+        return -1;
+    inpS = input('name:');
+    if not inpS:
+        print('please insert string..');
+        return -1;
+    size = int(input('size:'));
+    if size < 1:
+        print('please insert digit');
+        return -1;
+    color = input('color:');
+    if not color:
+        print('please insert color..');
+        return -1;
+    ch = int(input('channels:'));
+    if ch < 1:
+        print('please insert digit');
+        return -1;
+    if Type.lower() == "receiver":
+        Rec = Receiver(inpS, size, color, ch);
+        appendData(Rec);
+    if Type.lower() == "amplifier":
+        power = int(input('power:'));
+        if ch < 1:
+            print('please insert digit');
+            return -1;
+        Rec = Amplifier(inpS, power, ch, size);
+        appendData(Rec);
+
+def loadData():
+    with open('data.json', 'r') as f:
+        saveIn = json.load(f);
+        return saveIn;
+
+def appendData(Cate):
+    with open('data.json', 'r', encoding='utf-8') as file:
+        d = [];
+        # str = "";
+        try:
+            d = json.load(file);
+        except ValueError:
+            print('error here');
+        d.append(Cate.__dict__);
+        saveData(d);
+
+def saveData(data):
+    with open('data.json', 'w') as f:
+        saveIn = json.dumps(data);
+        f.write(saveIn);
+
+# def searchForType(data, targetName, targetType):
+#     for i in data:
+#         if i['type'] == targetType and i['name'] == targetName.lower():
+#             return True;
+#     return False;
+
+def deleteEntry(nameToDelete, typeToDelete):
+    try:
+        data = loadData();
+    except ValueError:
+            print('error here 2');
+    for idx, dictionary in enumerate(data):
+        if dictionary['type'].lower() == typeToDelete and dictionary['name'].lower() == nameToDelete.lower():
+            data.pop(idx)
+            saveData(data)
+            return;
+    print('entry %s not found', nameToDelete);
 
 
-def AddCategory():
-    inpI = int(input('''
-        type of product category:
-            for Receiver choose 1
-            For Amplifier choose 2
-            :
-    '''))
-    match inpI:
-        case 1:
-            inpS = input('category name:');
-            if not inpS:
-                print('please insert string..');
-                return -1;
-            size = int(input('category size:'));
-            if size < 1:
-                print('please insert digit');
-                return -1;
-            color = input('category color:');
-            if not color:
-                print('please insert color..');
-                return -1;
-            ch = int(input('category size:'));
-            if ch < 1:
-                print('please insert digit');
-                return -1;
-            Rec = Receiver(inpS, size, color, ch);
-            saveData(Rec);
 
-def saveData(Cate):
-    with open('data.json', 'a') as f:
-        print(Cate);
-        f.write(Cate);
+def RemoveProduct():
+    name = input('category name:');
+    if not name:
+        print('please insert string..');
+        return -1;
+    type = input('category type:');
+    if not type:
+        print('please insert color..');
+        return -1;
+    deleteEntry(name, type)
 
+def DisplayAllProduct():
+    print(loadData());
 
-def products():
+def Products():
     print('''
         choose option:
             1. Add a products
@@ -124,29 +153,11 @@ def products():
     option: int = input(':')
     match int(option):
         case 1:
-            pass
+            AddProduct();
         case 2:
-            pass
+            RemoveProduct();
         case 3:
-            pass
-
-
-def orders():
-    print('''
-        choose option:
-            1. Add a Orders
-            2. Remove a new Orders
-            3. Display all the Orders
-        ''')
-    option: int = input(':')
-    
-    match int(option):
-        case 1:
-            pass
-        case 2:
-            pass
-        case 3:
-            pass
+            DisplayAllProduct();
 
 
 
@@ -157,18 +168,14 @@ if __name__ == '__main__':
         print('''
         Sekelton Store:
         choose option:
-            1. Categories
-            2. Products
-            3. Orders
-            4. Exit
+            1. Products
+            2. Exit
         ''')
 
         option: int = input()
         match int(option):
             case 1:
-                catagories();
+                Products();
             case 2:
-                product();
-            case 3:
-                orders();
+                break;
             
